25c16ba8079f1d4c31d6e7af669d23cfb47e5ab0
Ebubekir <ebubekirc442@gmail.com>
2023-06-04 00:04:37 +0300

    commit08

---
+++ src/main/java/org/example/A/Tekrar.java
@@ -0,0 +1,21 @@
+package org.example.A;
+
+import java.sql.*;
+
+public class Tekrar {
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/jdbc_db", "dev_user", "password");
+        Statement st = con.createStatement();
+
+        //ÖRNEK 1:id'si 5 ile 10 arasında olan ülkelerin "country_name" bilgisini listeleyiniz.
+
+
+
+
+
+
+
+    }
+}
